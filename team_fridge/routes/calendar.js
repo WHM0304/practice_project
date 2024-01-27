7534e7302458e91fdfa65627781ddc3b4449e03a
WHM0304 <whm0304@naver.com>
2024-01-27 22:12:27 +0900

    js

--- team_fridge/routes/calendar.js
+++ team_fridge/routes/calendar.js
@@ -6,8 +6,10 @@ router.get("/", (req, res) => {
   return res.render("./calendar/calendar.pug");
 });
 
-router.get("/detail", (req, res) => {
-  return res.render("./calendar/detail.pug");
+router.get(`/:day/detail`, (req, res) => {
+  const param = req.params.day;
+
+  return res.render("./calendar/detail.pug", [param]);
 });
 
 export default router;
